db4a3fd103aad2486cf8a5d040f8e7bfe019397e
BohdanZyshchyk <bohdan.zyshchuk@zagravagames.com>
2020-09-07 16:53:34 +0300

    image

---
+++ MvcApplicationGameDemo/MvcApplicationDemo/GameStore.UI/Utils/ImageConfig.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace GameStore.UI.Utils
+{
+    public static class ImageConfig
+    {
+        public static string ProductImagePath
+        {
+            get { return ConfigurationManager.AppSettings["GameImagePath"]; }
+        }
+        public static string DomainProject
+        {
+            get { return ConfigurationManager.AppSettings["DomainProject"]; }
+        }
+    }
+}
